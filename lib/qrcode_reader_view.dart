31f7f5d7dceac9d09c1a630878df8488d946c9b8
VuDieuLinh96 <linh.vu@epay.vn>
2021-12-13 17:48:56 +0700

    Update giao diện qr code

--- lib/qrcode_reader_view.dart
+++ lib/qrcode_reader_view.dart
@@ -149,18 +149,17 @@ class QrcodeReaderViewState extends State<QrcodeReaderView>
               ),
             ),
             Positioned(
-              left: (constraints.maxWidth - qrScanSize) / 2,
-              top: (constraints.maxHeight - qrScanSize) / 2,
-              child: CustomPaint(
-                painter: QrScanBoxPainter(
-                  boxLineColor: widget.boxLineColor!,
-                  animationValue: _animationController.value,
-                  isForward:
-                      _animationController.status == AnimationStatus.forward,
-                ),
-                child: SizedBox(
-                  width: qrScanSize,
-                  height: qrScanSize,
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  decoration: ShapeDecoration(
+                    shape: QrScanBoxPainter(
+                      borderColor: Colors.white,
+                      borderRadius: 8,
+                      borderLength: 32,
+                      borderWidth: 4,
+                    ),
+                  ),
                 ),
               ),
             ),
@@ -210,100 +209,256 @@ class QrcodeReaderViewState extends State<QrcodeReaderView>
   }
 }
 
-class QrScanBoxPainter extends CustomPainter {
-  final double animationValue;
-  final bool isForward;
-  final Color? boxLineColor;
+// class QrScanBoxPainter extends CustomPainter {
 
-  QrScanBoxPainter(
-      {required this.animationValue,
-      required this.isForward,
-      this.boxLineColor})
-      // ignore: unnecessary_null_comparison
-      : assert(animationValue != null),
-        // ignore: unnecessary_null_comparison
-        assert(isForward != null);
+// final double animationValue;
+// final bool isForward;
+// final Color? boxLineColor;
+
+// QrScanBoxPainter(
+//     {required this.animationValue,
+//     required this.isForward,
+//     this.boxLineColor})
+//     // ignore: unnecessary_null_comparison
+//     : assert(animationValue != null),
+//       // ignore: unnecessary_null_comparison
+//       assert(isForward != null);
+
+// @override
+// void paint(Canvas canvas, Size size) {
+//   final borderRadius = BorderRadius.all(Radius.circular(12)).toRRect(
+//     Rect.fromLTWH(0, 0, size.width, size.height),
+//   );
+//   canvas.drawRRect(
+//     borderRadius,
+//     Paint()
+//       ..color = Colors.white54
+//       ..style = PaintingStyle.stroke
+//       ..strokeWidth = 1,
+//   );
+//   final borderPaint = Paint()
+//     ..color = Colors.white
+//     ..style = PaintingStyle.stroke
+//     ..strokeWidth = 2;
+//   final path = new Path();
+//   // leftTop
+//   path.moveTo(0, 50);
+//   path.lineTo(0, 12);
+//   path.quadraticBezierTo(0, 0, 12, 0);
+//   path.lineTo(50, 0);
+//   // rightTop
+//   path.moveTo(size.width - 50, 0);
+//   path.lineTo(size.width - 12, 0);
+//   path.quadraticBezierTo(size.width, 0, size.width, 12);
+//   path.lineTo(size.width, 50);
+//   // rightBottom
+//   path.moveTo(size.width, size.height - 50);
+//   path.lineTo(size.width, size.height - 12);
+//   path.quadraticBezierTo(
+//       size.width, size.height, size.width - 12, size.height);
+//   path.lineTo(size.width - 50, size.height);
+//   // leftBottom
+//   path.moveTo(50, size.height);
+//   path.lineTo(12, size.height);
+//   path.quadraticBezierTo(0, size.height, 0, size.height - 12);
+//   path.lineTo(0, size.height - 50);
+
+//   canvas.drawPath(path, borderPaint);
+
+//   canvas.clipRRect(
+//       BorderRadius.all(Radius.circular(12)).toRRect(Offset.zero & size));
+
+//   final linePaint = Paint();
+//   final lineSize = size.height * 0.45;
+//   final leftPress = (size.height + lineSize) * animationValue - lineSize;
+//   linePaint.style = PaintingStyle.stroke;
+//   linePaint.shader = LinearGradient(
+//     colors: [Colors.transparent, boxLineColor!],
+//     begin: isForward ? Alignment.topCenter : Alignment(0.0, 2.0),
+//     end: isForward ? Alignment(0.0, 0.5) : Alignment.topCenter,
+//   ).createShader(Rect.fromLTWH(0, leftPress, size.width, lineSize));
+//   for (int i = 0; i < size.height / 5; i++) {
+//     canvas.drawLine(
+//       Offset(
+//         i * 5.0,
+//         leftPress,
+//       ),
+//       Offset(i * 5.0, leftPress + lineSize),
+//       linePaint,
+//     );
+//   }
+//   for (int i = 0; i < lineSize / 5; i++) {
+//     canvas.drawLine(
+//       Offset(0, leftPress + i * 5.0),
+//       Offset(
+//         size.width,
+//         leftPress + i * 5.0,
+//       ),
+//       linePaint,
+//     );
+//   }
+// }
+
+// @override
+// bool shouldRepaint(QrScanBoxPainter oldDelegate) =>
+//     animationValue != oldDelegate.animationValue;
+
+// @override
+// bool shouldRebuildSemantics(QrScanBoxPainter oldDelegate) =>
+//     animationValue != oldDelegate.animationValue;
+// }
+
+const _CARD_ASPECT_RATIO = 3;
+const _OFFSET_X_FACTOR = 0.05;
+
+class QrScanBoxPainter extends ShapeBorder {
+  const QrScanBoxPainter({
+    this.borderColor = Colors.white,
+    this.borderWidth = 8.0,
+    this.overlayColor = const Color.fromRGBO(0, 0, 0, 80),
+    this.borderRadius = 12,
+    this.borderLength = 32,
+    this.cutOutBottomOffset = 0,
+  });
+
+  final Color borderColor;
+  final double borderWidth;
+  final Color overlayColor;
+  final double borderRadius;
+  final double borderLength;
+  final double cutOutBottomOffset;
 
   @override
-  void paint(Canvas canvas, Size size) {
-    final borderRadius = BorderRadius.all(Radius.circular(12)).toRRect(
-      Rect.fromLTWH(0, 0, size.width, size.height),
-    );
-    canvas.drawRRect(
-      borderRadius,
-      Paint()
-        ..color = Colors.white54
-        ..style = PaintingStyle.stroke
-        ..strokeWidth = 1,
-    );
-    final borderPaint = Paint()
-      ..color = Colors.white
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 2;
-    final path = new Path();
-    // leftTop
-    path.moveTo(0, 50);
-    path.lineTo(0, 12);
-    path.quadraticBezierTo(0, 0, 12, 0);
-    path.lineTo(50, 0);
-    // rightTop
-    path.moveTo(size.width - 50, 0);
-    path.lineTo(size.width - 12, 0);
-    path.quadraticBezierTo(size.width, 0, size.width, 12);
-    path.lineTo(size.width, 50);
-    // rightBottom
-    path.moveTo(size.width, size.height - 50);
-    path.lineTo(size.width, size.height - 12);
-    path.quadraticBezierTo(
-        size.width, size.height, size.width - 12, size.height);
-    path.lineTo(size.width - 50, size.height);
-    // leftBottom
-    path.moveTo(50, size.height);
-    path.lineTo(12, size.height);
-    path.quadraticBezierTo(0, size.height, 0, size.height - 12);
-    path.lineTo(0, size.height - 50);
+  EdgeInsetsGeometry get dimensions => const EdgeInsets.all(10);
 
-    canvas.drawPath(path, borderPaint);
-
-    canvas.clipRRect(
-        BorderRadius.all(Radius.circular(12)).toRRect(Offset.zero & size));
-
-    final linePaint = Paint();
-    final lineSize = size.height * 0.45;
-    final leftPress = (size.height + lineSize) * animationValue - lineSize;
-    linePaint.style = PaintingStyle.stroke;
-    linePaint.shader = LinearGradient(
-      colors: [Colors.transparent, boxLineColor!],
-      begin: isForward ? Alignment.topCenter : Alignment(0.0, 2.0),
-      end: isForward ? Alignment(0.0, 0.5) : Alignment.topCenter,
-    ).createShader(Rect.fromLTWH(0, leftPress, size.width, lineSize));
-    for (int i = 0; i < size.height / 5; i++) {
-      canvas.drawLine(
-        Offset(
-          i * 5.0,
-          leftPress,
-        ),
-        Offset(i * 5.0, leftPress + lineSize),
-        linePaint,
-      );
-    }
-    for (int i = 0; i < lineSize / 5; i++) {
-      canvas.drawLine(
-        Offset(0, leftPress + i * 5.0),
-        Offset(
-          size.width,
-          leftPress + i * 5.0,
-        ),
-        linePaint,
-      );
-    }
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()
+      ..fillType = PathFillType.evenOdd
+      ..addPath(getOuterPath(rect), Offset.zero);
   }
 
   @override
-  bool shouldRepaint(QrScanBoxPainter oldDelegate) =>
-      animationValue != oldDelegate.animationValue;
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    Path _getLeftTopPath(Rect rect) {
+      return Path()
+        ..moveTo(rect.left, rect.bottom)
+        ..lineTo(rect.left, rect.top)
+        ..lineTo(rect.right, rect.top);
+    }
+
+    return _getLeftTopPath(rect)
+      ..lineTo(
+        rect.right,
+        rect.bottom,
+      )
+      ..lineTo(
+        rect.left,
+        rect.bottom,
+      )
+      ..lineTo(
+        rect.left,
+        rect.top,
+      );
+  }
 
   @override
-  bool shouldRebuildSemantics(QrScanBoxPainter oldDelegate) =>
-      animationValue != oldDelegate.animationValue;
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    final offsetX = rect.width * _OFFSET_X_FACTOR;
+    final cardWidth = rect.width - _CARD_ASPECT_RATIO * offsetX;
+    final cardHeight = rect.width - _CARD_ASPECT_RATIO * offsetX;
+    final offsetY = (rect.height - cardHeight) / 2;
+
+    final backgroundPaint = Paint()
+      ..color = overlayColor
+      ..style = PaintingStyle.fill;
+
+    final borderPaint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = borderWidth;
+
+    final boxPaint = Paint()
+      ..color = borderColor
+      ..style = PaintingStyle.fill
+      ..blendMode = BlendMode.dstOut;
+
+    final cutOutRect = Rect.fromLTWH(
+      rect.left + offsetX,
+      rect.top + offsetY,
+      cardWidth,
+      cardHeight,
+    );
+
+    canvas
+      ..saveLayer(
+        rect,
+        backgroundPaint,
+      )
+      ..drawRect(
+        rect,
+        backgroundPaint,
+      )
+      // Draw top right corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.right - borderLength,
+          cutOutRect.top,
+          cutOutRect.right,
+          cutOutRect.top + borderLength,
+          topRight: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw top left corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.left,
+          cutOutRect.top,
+          cutOutRect.left + borderLength,
+          cutOutRect.top + borderLength,
+          topLeft: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw bottom right corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.right - borderLength,
+          cutOutRect.bottom - borderLength,
+          cutOutRect.right,
+          cutOutRect.bottom,
+          bottomRight: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      // Draw bottom left corner
+      ..drawRRect(
+        RRect.fromLTRBAndCorners(
+          cutOutRect.left,
+          cutOutRect.bottom - borderLength,
+          cutOutRect.left + borderLength,
+          cutOutRect.bottom,
+          bottomLeft: Radius.circular(borderRadius),
+        ),
+        borderPaint,
+      )
+      ..drawRRect(
+        RRect.fromRectAndRadius(
+          cutOutRect,
+          Radius.circular(borderRadius),
+        ),
+        boxPaint,
+      )
+      ..restore();
+  }
+
+  @override
+  ShapeBorder scale(double t) {
+    return QrScanBoxPainter(
+      borderColor: borderColor,
+      borderWidth: borderWidth,
+      overlayColor: overlayColor,
+    );
+  }
 }
